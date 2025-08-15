2c0bb6f39f1dfe233de6ea4ede8f0d120d2d90c5
basilcrunch <kbasilio@microsoft.com>
2025-08-15 14:44:38 -0700

    initial commit
    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript functionality can be added here if needed in the future.
+// For example, handling the resume upload button or adding interactivity to the photo gallery.
+
+console.log('Welcome to My Portfolio!');
